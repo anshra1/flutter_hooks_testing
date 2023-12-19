a3404147801c5a72bbfb891b674a71c2e93ba668
anshra1 <020ansh@gmail.com>
2023-12-19 21:50:42 +0530

    third Change

---
+++ lib/example_4/example4.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() async {
+
+runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blueGrey,
+        indicatorColor: Colors.blueGrey,
+      ),
+      home: const Text('Hello World'),
+    );
+  }
+}
